e8246da7ec0635e60cc3787534aa415a1ba113e2
rramola <ramolaryan@gmail.com>
2024-05-28 13:25:11 -0500

    moved scripts to script.js

---
+++ app/static/scripts.js
@@ -0,0 +1,26 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var usernameField = document.getElementById("id_username");
+    var firstNameField = document.getElementById("id_first_name");
+    var lastNameField = document.getElementById("id_last_name");
+    var emailField = document.getElementById("id_email");
+    var passOneField = document.getElementById("id_password1");
+    var passTwoField = document.getElementById("id_password2");
+    if (usernameField) {
+        usernameField.placeholder = "Username..";
+        }
+    if (firstNameField) {
+            firstNameField.placeholder = "First Name..";
+            }
+    if (lastNameField) {
+            lastNameField.placeholder = "Last Name..";
+            }
+    if (emailField) {
+        emailField.placeholder = "Email..";
+        }
+    if (passOneField){
+        passOneField.placeholder = "Password..";
+        }
+    if (passTwoField) {
+        passTwoField.placeholder = "Password..";
+        }
+    });
